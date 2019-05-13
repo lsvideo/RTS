a0516f6a12e1c4e6c6ca327f5669932e6f5b9647
lishuo <lishuo@shanlitech.com>
2019-05-13 11:20:16 +0800

    Add http+json post func.

---
+++ RTSClusterClient/httphelper.go
@@ -0,0 +1,35 @@
+// httphelper
+package main
+
+import (
+	"bytes"
+	//"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"unsafe"
+)
+
+func SamplePost(data string, url string) {
+	reader := bytes.NewReader([]byte(data))
+	request, err := http.NewRequest("POST", url, reader)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	request.Header.Set("Content-Type", "application/json;charset=UTF-8")
+	client := http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	//byte数组直接转成string，优化内存
+	str := (*string)(unsafe.Pointer(&respBytes))
+	fmt.Println(*str)
+}
