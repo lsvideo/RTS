16e5a734fac1640603c714d96ae6a9cbbc7597f1
lishuo <lishuo@shanlitech.com>
2019-05-16 11:36:55 +0800

    Do not panic when failed to open the log file.

--- sl_log/sl_log.go
+++ sl_log/sl_log.go
@@ -51,9 +51,8 @@ func SetLogPath(path string) {
 	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err == nil {
 		Log.Out = file
-		Log.Info("Open log file ", path)
+		Log.Infoln("Open log file ", path)
 	} else {
-		Log.Info("Failed to log to file, using default stderr")
-		panic(err)
+		Log.Warningln("Failed to log to file, using default stderr")
 	}
 }
