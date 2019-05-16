a3067d1daff89e5578c0d74ec8b9c03a0dd06015
lishuo <lishuo@shanlitech.com>
2019-05-16 11:52:43 +0800

    Add token func for zk interface.

---
+++ RTSClusterClient/token.go
@@ -0,0 +1,43 @@
+// token
+package main
+
+import (
+	"encoding/json"
+	"zkhelper"
+)
+
+type Token struct {
+	uid        string
+	cid        string
+	Token      string `json:"token"`      // token字符串
+	IP         string `json:"ip"`         //IP地址
+	length     int    `json:"length"`     //有效时长
+	CreateTime int32  `json:"createTime"` //创建时间
+}
+
+func (t *Token) GetToken(uid string) bool {
+	var res bool = true
+	var tokennode zkhelper.ZKNode //cluster manager服务 注册至自动发现节点
+	tokennode.SetServiceType(zkhelper.ServiceTypeRTMP)
+	tokennode.SetPath(zkhelper.GetNodePath(zkhelper.GetServicePath(tokennode.ServiceType), zkhelper.NodeTypeToken) + "/" + uid)
+	tokennode.SetName(uid)
+
+	str, err := rtsclient.Get(&tokennode)
+	if err != nil {
+		log.Warningln("Get token err:", err)
+		res = false
+	} else {
+		log.Warningln("Get token :", str)
+		err = json.Unmarshal([]byte(str), t)
+		if err != nil {
+			log.Warningln("Parse json token err:", err)
+			res = false
+		}
+	}
+	return res
+}
+
+func (t *Token) Verify(vt string, ip string) error {
+
+	return nil
+}
